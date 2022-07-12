0ddbc3725a484a152a092141a67d93368ba3b5c3
Nico M <nico_16bcn@hotmail.com>
2022-07-13 00:25:30 +0200

    hasta la iteración 3
    menuda fumada la iteración 3, falta que no se repitan los mismos actores

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import contacts from "./contacts.json";
+import "./App.css";
+import { useState } from "react";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  let randomContact = contacts[Math.floor(Math.random()*contacts.length)];//para hacer la iteracion 3(redondea,haceunrandom y multiplica por el largo del array)
+  let fiveContacts = contacts.slice(0,5)//da los primeros 5 valores del array
+                               //fiveContacts xa 1a iteracion//randomContact xa la 3a iteracion  
+  const [contactsArr, setContactsArr] = useState(fiveContacts,randomContact);
+  //iteracion-2-metodo-1 Forma de Burro(como yo). Usando ternarios es mas facil.
+  // const renderOscar = (contact) => {
+    //   if (contact.wonOscar) {
+      //     return '🏆';                              
+      //   } 
+      // };
+      // const renderEmmy = (contact) => {
+        //   if (contact.wonEmmy) {
+          //     return '🏆';
+          //   } 
+          // };
+          
+          //iteración 3
+          const addRandomContact = () => {
+            if (contactsArr.includes(randomContact)) {
+              randomContact = contactsArr[Math.floor(Math.random() * contactsArr.length)];
+            }
+            setContactsArr([...contactsArr,randomContact ]) 
+         //iteración3
+  };
+  
+  return <div className="App">
+  <button onClick={ () => addRandomContact() } className="btn-random">
+    Add random
+  </button>
+    <table>
+      <thead>
+        <tr>
+          <th>picture</th>
+          <th>name</th>
+          <th>popularity</th>
+          <th>wonOscar</th>
+          <th>wonEmmy</th>
+        </tr>
+      </thead>
+      
+      <tbody>
+      {contactsArr.map((thisContact) => {
+          return (         
+        <tr>
+          <td><img src={thisContact.pictureUrl} alt=""></img> </td>
+          <td>{thisContact.name}</td>
+          <td>{thisContact.popularity}</td>
+          {/* iteracion-2-metodo-1 usando la funcion de arriba */}
+          {/* <td>{renderOscar(thisContact)}</td>
+          <td>{renderEmmy(thisContact)}</td> */}
+          <td>{thisContact.wonOscar ?  '🏆' : ' '}</td>     
+                                                        {/* iteracion-2-metodo-2(mas facil) Usando ternarios */}
+          <td>{thisContact.wonEmmy ?  '🏆' : ' '}</td>       
+        </tr>
+           
+          );
+        })
+      }
+      </tbody>
+    </table>
+  </div>;
 }
-
 export default App;
+
